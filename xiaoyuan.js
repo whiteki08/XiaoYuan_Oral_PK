beb316fe76b9d9426c27fab3278b4207bbd1a17d
whiteki08 <78333781+whiteki08@users.noreply.github.com>
2024-10-10 13:48:41 +0800

    Create xiaoyuan.js

---
+++ xiaoyuan.js
@@ -0,0 +1,18 @@
+/**
+ * @fileoverview Script to modify JSON response
+ */
+
+let body = $response.body;
+let obj = JSON.parse(body);
+
+function modifyAnswers(questions) {
+    for (let question of questions) {
+        question.answer = "1";
+    }
+}
+
+if (obj.examVO && obj.examVO.questions) {
+    modifyAnswers(obj.examVO.questions);
+}
+
+$done({ body: JSON.stringify(obj) });
